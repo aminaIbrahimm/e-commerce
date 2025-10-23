c125230cec7b99d92bb54da2206052d2bb9c24e9
aminaibrahimm <ai7687772@gmail.com>
2025-10-23 03:32:59 +0200

    add UI and responsive in login, nav, footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import Style from './Footer.module.css'
+import React from 'react'
 import amazon from '../../assets/Amazon_Pay-Logo.wine.svg'
-import american from '../../assets/American-Express-Color.png'
 import paypal from '../../assets/PayPal.svg.webp'
 import mastercard from '../../assets/MasterCard_early_1990s_logo.png'
 import applelogo from '../../assets/Download_on_the_App_Store_Badge.svg.webp'
 import googlelogo from '../../assets/Google_Play_Store_badge_EN.svg.webp'
 export default function Footer() {
-  return <>
-  <div className=' bg-gray-300 start-0 end-0 p-8 bottom-0'>
-    <h2 className='capitalize text-2xl px-4 md:px-0'>get the freshCart app</h2>
-      <p className='text-gray-400 px-4 md:px-0'>We will send you a link, open it on your phone to download the app.</p>
-      <div className="flex  px-8 my-5 space-x-3">
-      <div className="w-full">
-        <input type="email" id="email" className="outline-none
+  return (
+    <>
+      <div className=" bg-teal-900  start-0 end-0 p-8 bottom-0 w-full ">
+        <h2 className="capitalize text-2xl px-4 md:px-0">
+          get the freshCart app
+        </h2>
+        <p className="text-gray-400 px-4 md:px-0">
+          We will send you a link, open it on your phone to download the app.
+        </p>
+        <div className="flex px-8 my-5 space-x-3">
+          <div className="w-full">
+            <input
+              type="link"
+              id="link"
+              className="outline-none
         w-full
         shadow-xs
         bg-white
@@ -22,7 +28,7 @@ export default function Footer() {
           text-gray-900 
           text-sm rounded-lg
           focus:ring-blue-500
-            focus:border-blue-500
+            focus:border-green-500
             block
               p-2.5 
               
@@ -31,26 +37,33 @@ export default function Footer() {
                 
                 dark:focus:ring-blue-500 
                 dark:focus:border-blue-500 
-                dark:shadow-xs-light" placeholder="Email..." />
-      </div>
-      <button type="button" className="xl:w-1/7 lg:w-1/5 md:w-1/4 sm:w-1/3 w-2/3 outline-none capitalize cursor-pointer  text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-sm px-2 py-1.5 me-2  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">share app link</button>
-      </div>
-    <hr className='text-gray-400 px-8' />
-      <div className="flex items-center justify-between container mx-auto">
-        <div className='flex items-center gap-2'>
-          <h3 className='capitalize'>payment partners</h3>
-          <img src={amazon} alt="" className='w-22 me-1'/>
-          <img src={paypal} alt="" className='w-15 me-1'/>
-          <img src={mastercard} alt="" className='w-15 me-1'/>
-          <img src={american} alt="" className='w-15 me-1'/>
+                dark:shadow-xs-light"
+              placeholder="Email..."
+            />
+          </div>
+          <button
+            type="button"
+            className="xl:w-1/7 lg:w-1/5 md:w-1/4 sm:w-1/3 w-2/3 outline-none capitalize cursor-pointer  text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-sm px-2 py-1.5 me-2  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            share app link
+          </button>
         </div>
-        <div className='flex items-center gap-2'>
-          <h3 className='capitalize'>get delevries with fresh cart</h3>
-          <img src={applelogo} alt="" className='w-20 me-1'/>
-          <img src={googlelogo} alt="" className='w-20 me-1'/>
+        <hr className="text-gray-400 px-8"/>
+        <div className="flex flex-col sm-flex-row items-center justify-between container mx-auto">
+          <div className="flex items-center gap-2">
+            <h3 className="capitalize">payment partners</h3>
+            <img src={amazon} alt="amazon" className="w-22 me-1" />
+            <img src={paypal} alt="paypal" className="w-15 me-1" />
+            <img src={mastercard} alt="mastercard" className="w-15 me-1" />
+          </div>
+          <div className="flex items-center gap-2">
+            <h3 className="capitalize">get delevries with fresh cart</h3>
+            <img src={applelogo} alt="" className="w-20 me-1" />
+            <img src={googlelogo} alt="" className="w-20 me-1" />
+          </div>
         </div>
+        <hr className="text-gray-400 px-8" />
       </div>
-    <hr className='text-gray-400 px-8' />
-  </div>
-  </>
+    </>
+  );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, {useContext, useEffect, useState} from 'react'
-import Style from './Navbar.module.css'
+import React, {useContext, useState} from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
 import { authContext } from '../../Context/AuthContextProvider'
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
   let {token,setToken} = useContext(authContext)
   let navigate = useNavigate("token")
   const location = useLocation();
@@ -13,32 +13,183 @@ export default function Navbar() {
     localStorage.removeItem("token")
     navigate("/login")
   }
-  return <>
-    <nav className='bg-gray-200 py-3 fixed w-full z-[999] overflow-auto top-0'>
-      <div className="container mx-auto flex items-center">
-        <Link to="/">
-          <img src={logo} alt="" />
-        </Link>
-        {token ? <ul className='flex gap-3 ms-4'>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/products'>products</Link></li>
-          <li><Link to='/wishlist'>wishlist</Link></li>
-          <li><Link to='/cart'>cart</Link></li>
-          <li><Link to='/categories'>categories</Link></li>
-          <li><Link to='/brand'>brand</Link></li>
-        </ul> : null}
-        <ul className='flex gap-3 ms-auto'>
-          <li><i className="fa-brands fa-facebook-f"></i></li>
-          <li><i className="fa-brands fa-tiktok"></i></li>
-          <li><i className="fa-brands fa-twitter"></i></li>
-          <li><i className="fa-brands fa-yahoo"></i></li>
-          <li><i className="fa-brands fa-github"></i></li>
-          {token ? <li><span onClick={()=>logout()}>Logout</span></li> : <>
-            <li><Link className={`hover:text-green-600 ${location.pathname === "/login" ? "text-green-600 font-bold" : ""}`} to='/login'>Login</Link></li>
-            <li><Link className={`hover:text-green-600 ${location.pathname === "/register" ? "text-green-600 font-bold" : ""}`} to='/register'>Register</Link></li>
-          </>}
-        </ul>
-      </div>
-    </nav>
- </>
+  
+  return (
+    <>
+      <nav className="bg-teal-600 w-full z-20 top-0 start-0 fixed ">
+        <div className="relative max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link to="/">
+            <img src={logo} alt="Logo Fresh cart" />
+          </Link>
+          <div className="flex space-x-3 md:space-x-0 rtl:space-x-reverse items-center">
+            <ul className="flex flex-col md:flex-row gap-3 ms-auto">
+              <ul className="flex gap-3">
+                <li>
+                  <i className="fa-brands fa-facebook-f cursor-pointer hover:text-green-500  "></i>
+                </li>
+                <li>
+                  <i className="fa-brands fa-tiktok cursor-pointer hover:text-green-500 "></i>
+                </li>
+                <li>
+                  <i className="fa-brands fa-twitter cursor-pointer hover:text-green-500 "></i>
+                </li>
+                <li>
+                  <i className="fa-brands fa-yahoo cursor-pointer hover:text-green-500 "></i>
+                </li>
+                <li>
+                  <i className="fa-brands fa-github cursor-pointer hover:text-green-500 "></i>
+                </li>
+              </ul>
+              {token ? (
+                <li className="text-end">
+                  <span
+                    onClick={() => logout()}
+                    className="cursor-pointer hover:text-green-500"
+                  >
+                    Logout
+                  </span>
+                </li>
+              ) : (
+                <>
+                  <ul className="flex gap-3">
+                    <li>
+                      <Link
+                        className={`hover:text-green-500 ${
+                          location.pathname === "/login"
+                            ? "text-green-500 font-bold"
+                            : ""
+                        }`}
+                        to="/login"
+                      >
+                        Login
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className={`hover:text-green-500 ${
+                          location.pathname === "/register"
+                            ? "text-green-500 font-bold"
+                            : ""
+                        }`}
+                        to="/register"
+                      >
+                        Register
+                      </Link>
+                    </li>
+                  </ul>
+                </>
+              )}
+            </ul>
+            {token && (
+              <button
+                onClick={() => setOpen(!open)}
+                data-collapse-toggle="navbar-sticky"
+                type="button"
+                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 "
+                aria-controls="navbar-sticky"
+                aria-expanded={open}
+              >
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </button>
+            )}
+
+            <div
+              id="navbar-sticky"
+              className={`${
+                open ? "block" : "hidden"
+              }  w-full md:block md:w-auto md:absolute md:left-[50%] md:top-[20%]`}
+            >
+              {token && (
+                <ul className="absolute left-0 top-full w-full bg-teal-600 shadow-md rounded-b-lg flex flex-col md:p-0 font-medium md:flex-row md:mt-0 justify-center">
+                  <li>
+                    <Link
+                      to="/"
+                      className={`block py-2 px-3 rounded-sm ${
+                        location.pathname === "/" ? "md:text-green-700" : ""
+                      }`}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/products"
+                      className={`block py-2 px-3 rounded-sm ${
+                        location.pathname === "/products"
+                          ? "md:text-green-700"
+                          : ""
+                      }`}
+                    >
+                      Products
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/wishlist"
+                      className={`block py-2 px-3 rounded-sm ${
+                        location.pathname === "/wishlist"
+                          ? "md:text-green-700"
+                          : ""
+                      }`}
+                    >
+                      Wishlist
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/cart"
+                      className={`block py-2 px-3 rounded-sm ${
+                        location.pathname === "/cart" ? "md:text-green-700" : ""
+                      }`}
+                    >
+                      Cart
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/categories"
+                      className={`block py-2 px-3 rounded-sm ${
+                        location.pathname === "/categories"
+                          ? "md:text-green-700"
+                          : ""
+                      }`}
+                    >
+                      Categories
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/brand"
+                      className={`block py-2 px-3 rounded-sm ${
+                        location.pathname === "/brand"
+                          ? "md:text-green-700"
+                          : ""
+                      }`}
+                    >
+                      Brand
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
